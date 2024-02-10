b63dc7476d0d961790a4d6b873f8a8572ef2c59b
Henry Bennett <henry.bennett@mymail.champlain.edu>
2024-02-10 16:58:13 -0500

    tile and node script push

--- Contin A Star/Assets/Scripts/Nodes.cs
+++ Contin A Star/Assets/Scripts/Nodes.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class Nodes : MonoBehaviour
 {
+    public Tile currentTile;
+    private int floorX;
+    private int floorY;
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentTile = new Tile();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        floorX = Mathf.FloorToInt(transform.position.x);
+        floorY = Mathf.FloorToInt(transform.position.y);
+        if (transform.position.x - floorX <= 0.5f)
+        {
+            currentTile.currentPos.x = floorX;
+        }
+        else
+        {
+            currentTile.currentPos.x = floorX + 1;
+        }
+        if (transform.position.y - floorY <= 0.5f)
+        {
+            currentTile.currentPos.y = floorY;
+        }
+        else
+        {
+            currentTile.currentPos.y = floorY + 1;
+        }
+
+        Debug.Log(currentTile.currentPos);
     }
 }
