944b94d3b50c3e78044f2fbe7ae5122853cb8cac
S_Shomo <90357187+SShomo@users.noreply.github.com>
2024-02-12 17:07:19 -0500

    test2

--- Contin A Star/Assets/Scripts/TextFields.cs
+++ Contin A Star/Assets/Scripts/TextFields.cs
@@ -1,22 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 using static Unity.VisualScripting.Member;
 
 public class TextFields : MonoBehaviour
 {
-    [SerializeField] private Text xSource;
-    [SerializeField] private Text ySource;
-    [SerializeField] private Text xDes;
-    [SerializeField] private Text yDes;
+    [SerializeField] private TextMeshProUGUI xSource;
+    [SerializeField] private TextMeshProUGUI ySource;
+    [SerializeField] private TextMeshProUGUI xDes;
+    [SerializeField] private TextMeshProUGUI yDes;
 
     Node source = new Node();
     Node des = new Node();
 
     private void Update()
     {
-       // source.currentTile.currentPos.x = (float)xSource.text;
+        try
+        {
+            source.currentTile.currentPos.x = float.Parse(xSource.text);
+            source.currentTile.currentPos.y = float.Parse(ySource.text);
 
+            des.currentTile.currentPos.x = float.Parse(xDes.text);
+            des.currentTile.currentPos.y = float.Parse(yDes.text);
+
+        }
+        catch
+        {
+            source.currentTile.currentPos.x = 0;
+            source.currentTile.currentPos.y = 0;
+
+            des.currentTile.currentPos.x = 5;
+            des.currentTile.currentPos.y = 5;
+        }
     }
 }
